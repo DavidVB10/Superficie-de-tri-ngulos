cbd928490aa2807a9140c0b101ef9d3a98c89dcd
David Villa Blanco <davidvillablanco@MacBook-Pro-de-David.local>
2018-10-03 00:24:49 +0200

    mi primer commit

---
+++ Superficie de cuadrados.py
@@ -0,0 +1,3 @@
+x=input("Introduzca la medida en cm del lado del cuadrado = ")
+print("La superficie del cuadrado es de " + str(x*x) + " cm")
+
---
+++ Superficie de triángulos.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+b=input("Introduzca la medida en cm de la base del triángulo = ")
+h=input("Introduzca la medida en cm de la altura del triángulo = ")
+print ("La superficie del triángulo es de " + str((b*h)/2) + " cm2")
